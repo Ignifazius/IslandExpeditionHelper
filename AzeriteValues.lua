b3783e2ad14c0d19d3c258223a81d00d50b2093b
Ignifazius <stefan.hobohm@gmail.com>
2019-10-06 12:36:11 +0200

    externalized azerite table and store difficulties instead of deriving them

---
+++ AzeriteValues.lua
@@ -0,0 +1,146 @@
+local name, addon = ...;
+
+addon.values = {
+	["Meeting Stone"] = {
+		["Normal"] = 0,
+		[""] = 10
+		},
+	["Lord Gazzran"] = {
+		["Normal"] = 325,
+	},
+	["Azergem Crystalback"] = {
+		["Normal"] = 120,
+	},
+	["Mistscorn Ravager"] = {
+		["Normal"] = 12,
+	},
+	["Mistscorn Earthbinder"] = {
+		["Normal"] = 12,
+	},
+	["Volcano"] = {
+		["Normal"] = 0,
+	},
+	["Enslaved Rock Elemental"] = {
+		["Normal"] = 3,
+	},
+	["Pouch of Azerite"] = {
+		["Normal"] = 45,
+	},
+	["Risen Icetusk Brute"] = {
+		["Normal"] = 12,
+	},
+	["Icetusk Shadowcaster"] = {
+		["Normal"] = 6,
+	},
+	["Death-Caller Majuli"] = {
+		["Normal"] = 175,
+	},
+	["Mistscorn Sharphorn"] = {
+		["Normal"] = 6,
+	},
+	["Fire Lord's Cache"] = {
+		["Normal"] = 150,
+	},
+	["Old Chest"] = {
+		["Normal"] = 80,
+	},
+	["Mistscorn Marauder"] = {
+		["Normal"] = 90,
+	},
+	["Icetusk Defender"] = {
+		["Normal"] = 12,
+	},
+	["Frostscale Hydra"] = {
+		["Normal"] = 12,
+	},
+	["Droplet"] = {
+		["Normal"] = 3,
+	},
+	["Icetusk Necromancer"] = {
+		["Normal"] = 12,
+	},
+	["Tidal Surger"] = {
+		["Normal"] = 12,
+	},
+	["Flamescale Strider"] = {
+		["Normal"] = 90,
+	},
+	["Azerite Fissure"] = {
+		["Normal"] = 200,
+	},
+	["Butun the Boneripper"] = {
+		["Normal"] = 175,
+	},
+	["Geoactive Azershard"] = {
+		["Normal"] = 18,
+	},
+	["Wooden Strongbox"] = {
+		["Normal"] = 45,
+	},
+	["Enslaved Frost Elemental"] = {
+		["Normal"] = 6,
+	},
+	["Mistscorn Subjugator"] = {
+		["Normal"] = 90,
+	},
+	["Icetusk Drudge"] = {
+		["Normal"] = 3,
+	},
+	["Unleashed Inferno"] = {
+		["Normal"] = 90,
+	},
+	["Venture Goon"] = {
+		["Normal"] = 6,
+	},
+	["Machitu the Brutal"] = {
+		["Normal"] = 175,
+	},
+	["Water Elemental"] = {
+		["Normal"] = 6,
+	},
+	["Slimeskin Toad"] = {
+		["Normal"] = 12,
+	},
+	["Bag of Azerite"] = {
+		["Normal"] = 80,
+	},
+	["Safety Inspection Bot"] = {
+		["Normal"] = 3,
+	},
+	["Mechanical Guardhound"] = {
+		["Normal"] = 6,
+	},
+	["Darktunnel Ambusher"] = {
+		["Normal"] = 12,
+	},
+	["Azerite Crystal"] = {
+		["Normal"] = 135,
+	},
+	["Venture Surveyor"] = {
+		["Normal"] = 12,
+	},
+	["Icetusk Prophet"] = {
+		["Normal"] = 90,
+	},
+	["Matriarch Nas'naya"] = {
+		["Normal"] = 325,
+	},
+	["Reinforced Chest"] = {
+		["Normal"] = 115,
+	},
+	["Unstable Typhoon"] = {
+		["Normal"] = 90,
+	},
+	["Azergem Shardback"] = {
+		["Normal"] = 18,
+	},
+	["Senior Producer Gixi"] = {
+		["Normal"] = 325,
+	},
+	["Deepbore"] = {
+		["Normal"] = 175,
+	},
+	["Deathwhisperer Kulu"] = {
+		["Normal"] = 325,
+	},
+}
