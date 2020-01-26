9d4f306e58c1f4641e71bf52fb9070d434d3f6e4
Stefan Hobohm <stefan.hobohm@gmail.com>
2020-01-26 18:14:56 +0100

    more azerite values

--- AzeriteValues.lua
+++ AzeriteValues.lua
@@ -1,146 +1,662 @@
 local name, addon = ...;
 
 addon.values = {
-	["Meeting Stone"] = {
-		["Normal"] = 0,
-		[""] = 10
-		},
-	["Lord Gazzran"] = {
-		["Normal"] = 325,
+	["Bloodfist Elementalist"] = {
+		["Normal"] = 90,
 	},
-	["Azergem Crystalback"] = {
-		["Normal"] = 120,
+	["Bramblefur Calf"] = {
+		["Heroic"] = 5,
 	},
-	["Mistscorn Ravager"] = {
+	["Icetusk Warrior"] = {
+		["Heroic"] = 9,
+	},
+	["Mo'gosh Warmonger"] = {
+		["Normal"] = 90,
+	},
+	["Fleshrot Mystic"] = {
+		["Heroic"] = 18,
+	},
+	["Rustpelt Snarler"] = {
 		["Normal"] = 12,
 	},
-	["Mistscorn Earthbinder"] = {
+	["Mo'gosh Mauler"] = {
 		["Normal"] = 12,
 	},
-	["Volcano"] = {
-		["Normal"] = 0,
+	["Encrusted Coralback"] = {
+		["Heroic"] = 135,
 	},
-	["Enslaved Rock Elemental"] = {
-		["Normal"] = 3,
+	["Mogu Chest"] = {
+		["Heroic"] = 173,
+		["Normal"] = 115,
 	},
-	["Pouch of Azerite"] = {
-		["Normal"] = 45,
+	["Cragtusk"] = {
+		["Heroic"] = 263,
 	},
-	["Risen Icetusk Brute"] = {
-		["Normal"] = 12,
-	},
-	["Icetusk Shadowcaster"] = {
-		["Normal"] = 6,
-	},
-	["Death-Caller Majuli"] = {
-		["Normal"] = 175,
-	},
-	["Mistscorn Sharphorn"] = {
-		["Normal"] = 6,
-	},
-	["Fire Lord's Cache"] = {
-		["Normal"] = 150,
+	["Broodwatcher Anub'akar"] = {
+		["Heroic"] = 488,
 	},
 	["Old Chest"] = {
+		["Heroic"] = 120,
 		["Normal"] = 80,
 	},
-	["Mistscorn Marauder"] = {
-		["Normal"] = 90,
+	["Bloodscent the Tracker"] = {
+		["Normal"] = 325,
 	},
-	["Icetusk Defender"] = {
-		["Normal"] = 12,
+	["Twilight Scalesworn"] = {
+		["Heroic"] = 18,
 	},
-	["Frostscale Hydra"] = {
-		["Normal"] = 12,
+	["Shadowspeaker Angolo"] = {
+		["Heroic"] = 263,
 	},
-	["Droplet"] = {
-		["Normal"] = 3,
-	},
-	["Icetusk Necromancer"] = {
-		["Normal"] = 12,
-	},
-	["Tidal Surger"] = {
-		["Normal"] = 12,
-	},
-	["Flamescale Strider"] = {
-		["Normal"] = 90,
+	["Needleback Forager"] = {
+		["Heroic"] = 9,
 	},
 	["Azerite Fissure"] = {
+		["Heroic"] = 300,
 		["Normal"] = 200,
 	},
-	["Butun the Boneripper"] = {
-		["Normal"] = 175,
+	["Twilight Dragonkin"] = {
+		["Heroic"] = 9,
 	},
 	["Geoactive Azershard"] = {
 		["Normal"] = 18,
+		["Heroic"] = 27,
 	},
-	["Wooden Strongbox"] = {
-		["Normal"] = 45,
+	["Water Spirit"] = {
+		["Heroic"] = 9,
 	},
-	["Enslaved Frost Elemental"] = {
+	["Greatfangs"] = {
+		["Heroic"] = 263,
+	},
+	["Goldspine"] = {
+		["Heroic"] = 263,
+	},
+	["Detected Azerite Pocket"] = {
+		["Heroic"] = 0,
+	},
+	["Heliodor"] = {
+		["Heroic"] = 263,
+	},
+	["Volcano"] = {
+		["Heroic"] = 0,
+	},
+	["Swifttail Scavenger"] = {
 		["Normal"] = 6,
 	},
-	["Mistscorn Subjugator"] = {
-		["Normal"] = 90,
+	["Fleshrot Runeweaver"] = {
+		["Heroic"] = 135,
 	},
-	["Icetusk Drudge"] = {
+	["Acidic Burrower"] = {
+		["Heroic"] = 18,
+	},
+	["Slitherblade Oracle"] = {
+		["Heroic"] = 18,
+	},
+	["Bloodfist Ogre"] = {
 		["Normal"] = 3,
 	},
-	["Unleashed Inferno"] = {
-		["Normal"] = 90,
+	["Giddyleaf"] = {
+		["Heroic"] = 263,
 	},
-	["Venture Goon"] = {
-		["Normal"] = 6,
+	["Animated Azershard"] = {
+		["Heroic"] = 5,
+		["Normal"] = 3,
 	},
-	["Machitu the Brutal"] = {
+	["Brimstone Tracker"] = {
+		["Normal"] = 12,
+	},
+	["Dark Water"] = {
 		["Normal"] = 175,
 	},
-	["Water Elemental"] = {
-		["Normal"] = 6,
+	["Reinforced Chest"] = {
+		["Heroic"] = 173,
+		["Normal"] = 115,
 	},
-	["Slimeskin Toad"] = {
+	["Necromancer Conjurer"] = {
+		["Heroic"] = 18,
+	},
+	["Palefur Devourer"] = {
+		["Normal"] = 175,
+	},
+	["Bramblefur Grazer"] = {
+		["Heroic"] = 18,
+	},
+	["Broodwatcher Tal'nadix"] = {
+		["Heroic"] = 488,
+	},
+	["Mudsnout Gorer"] = {
+		["Heroic"] = 18,
+	},
+	["Burgle"] = {
+		["Heroic"] = 525,
+	},
+	["Thirsting Bloodsucker"] = {
 		["Normal"] = 12,
 	},
-	["Bag of Azerite"] = {
-		["Normal"] = 80,
+	["Slitherblade Wavecaller"] = {
+		["Heroic"] = 9,
 	},
-	["Safety Inspection Bot"] = {
-		["Normal"] = 3,
+	["Frostscale Wanderer"] = {
+		["Heroic"] = 135,
 	},
-	["Mechanical Guardhound"] = {
-		["Normal"] = 6,
+	["Gritplate Basilisk"] = {
+		["Heroic"] = 9,
 	},
-	["Darktunnel Ambusher"] = {
-		["Normal"] = 12,
+	["Vyliss"] = {
+		["Heroic"] = 263,
 	},
-	["Azerite Crystal"] = {
-		["Normal"] = 135,
+	["Slitherblade Skulker"] = {
+		["Heroic"] = 18,
 	},
-	["Venture Surveyor"] = {
-		["Normal"] = 12,
+	["Territorial Needleback"] = {
+		["Heroic"] = 18,
 	},
-	["Icetusk Prophet"] = {
-		["Normal"] = 90,
+	["Voru'kar Swarmguard"] = {
+		["Heroic"] = 135,
 	},
-	["Matriarch Nas'naya"] = {
+	["Deathsting Lasher"] = {
+		["Heroic"] = 18,
+	},
+	["Needleback Brute"] = {
+		["Heroic"] = 135,
+	},
+	["High Shaman Morg"] = {
 		["Normal"] = 325,
 	},
-	["Reinforced Chest"] = {
-		["Normal"] = 115,
+	["Ashstone"] = {
+		["Heroic"] = 488,
+	},
+	["Stonebound Shale-Speaker"] = {
+		["Heroic"] = 135,
+	},
+	["Forked-Tongue"] = {
+		["Heroic"] = 263,
+	},
+	["Firesting Drone"] = {
+		["Heroic"] = 9,
+	},
+	["Coagulated Azerite"] = {
+		["Heroic"] = 5,
+		["Normal"] = 3,
+	},
+	["Mogu Hoard"] = {
+		["Heroic"] = 225,
+	},
+	["Void Maggot"] = {
+		["Normal"] = 3,
+	},
+	["Scarpaw"] = {
+		["Heroic"] = 263,
+	},
+	["Necromancer Disciple"] = {
+		["Heroic"] = 9,
+	},
+	["Twilight Scalesister"] = {
+		["Heroic"] = 9,
+	},
+	["Konrad the Enslaver"] = {
+		["Heroic"] = 263,
+	},
+	["Nightscale Coilfang"] = {
+		["Heroic"] = 135,
+		["Normal"] = 90,
+	},
+	["Spineclaw Sandsnapper"] = {
+		["Heroic"] = 5,
+	},
+	["Mammoth Stonehide"] = {
+		["Heroic"] = 135,
+	},
+	["Twilight Commander"] = {
+		["Heroic"] = 135,
+	},
+	["Icetusk Drudge"] = {
+		["Heroic"] = 5,
+	},
+	["Spellbinder Ulura"] = {
+		["Heroic"] = 263,
+	},
+	["Zian-Ti Quilen"] = {
+		["Normal"] = 6,
+	},
+	["Fleshrot Blighthound"] = {
+		["Heroic"] = 18,
+	},
+	["Rustpelt Pup"] = {
+		["Normal"] = 3,
+	},
+	["Mudsnout Thornback"] = {
+		["Heroic"] = 135,
+	},
+	["Frostscale Broodmother"] = {
+		["Heroic"] = 488,
+	},
+	["Leatherwing Hunter"] = {
+		["Normal"] = 6,
+	},
+	["Mudsnout Piglet"] = {
+		["Heroic"] = 5,
+	},
+	["Zian-Ti Darkweaver"] = {
+		["Normal"] = 90,
+	},
+	["Deepsea Crab"] = {
+		["Heroic"] = 9,
+	},
+	["Deepsea Sandcrawler"] = {
+		["Heroic"] = 18,
+	},
+	["Swifttail Courser"] = {
+		["Normal"] = 12,
+	},
+	["Overseer of Twilight"] = {
+		["Heroic"] = 488,
+	},
+	["Runehoof Calf"] = {
+		["Heroic"] = 5,
+	},
+	["Gritplate Gazer"] = {
+		["Normal"] = 12,
+		["Heroic"] = 18,
+	},
+	["Azergem Crawler"] = {
+		["Heroic"] = 15,
+	},
+	["Brineshell Seacaller"] = {
+		["Heroic"] = 9,
+	},
+	["Azergem Shardback"] = {
+		["Heroic"] = 27,
+		["Normal"] = 18,
+	},
+	["Twilight Whelp"] = {
+		["Heroic"] = 5,
+	},
+	["Garnetback Striker"] = {
+		["Heroic"] = 18,
+	},
+	["Fiery Chest"] = {
+		["Heroic"] = 173,
+	},
+	["Stonehide Bull"] = {
+		["Heroic"] = 18,
+	},
+	["Blazeseeker"] = {
+		["Normal"] = 325,
+	},
+	["Nightscale Hatchling"] = {
+		["Normal"] = 3,
+	},
+	["Volatile Azerite"] = {
+		["Heroic"] = 180,
+		["Normal"] = 120,
+	},
+	["Acidic Worm"] = {
+		["Heroic"] = 9,
+	},
+	["Summoner Laniella"] = {
+		["Heroic"] = 263,
+	},
+	["Monstrous Spineclaw"] = {
+		["Heroic"] = 135,
+	},
+	["Skeleton Guardian"] = {
+		["Heroic"] = 135,
+	},
+	["Faceless One"] = {
+		["Normal"] = 12,
+	},
+	["Rumbling Earth"] = {
+		["Heroic"] = 5,
+	},
+	["Necromancer Master"] = {
+		["Heroic"] = 135,
+		["Normal"] = 90,
+	},
+	["Spiked Ghoul"] = {
+		["Heroic"] = 18,
+		["Normal"] = 12,
+	},
+	["Blazing Spark"] = {
+		["Heroic"] = 5,
+	},
+	["Knucklebump Stomper"] = {
+		["Normal"] = 12,
+	},
+	["Icetusk Blood-Drinker"] = {
+		["Heroic"] = 135,
+	},
+	["Gholvran the Cryptic"] = {
+		["Heroic"] = 263,
+	},
+	["Zian-Ti Spirit"] = {
+		["Normal"] = 6,
+	},
+	["Gemshard Colossus"] = {
+		["Normal"] = 325,
+	},
+	["Firesting Dominator"] = {
+		["Heroic"] = 135,
+	},
+	["Twilight Drakonaar"] = {
+		["Heroic"] = 135,
+	},
+	["Azerite Crystal"] = {
+		["Heroic"] = 203,
+		["Normal"] = 135,
+	},
+	["Voru'kar Flyer"] = {
+		["Heroic"] = 5,
+	},
+	["Icetusk Defender"] = {
+		["Heroic"] = 18,
+	},
+	["Voru'kar Spitter"] = {
+		["Heroic"] = 9,
+	},
+	["Zess'ez"] = {
+		["Heroic"] = 263,
+	},
+	["Smolderheart"] = {
+		["Heroic"] = 263,
+	},
+	["Icetusk Necromancer"] = {
+		["Heroic"] = 18,
+	},
+	["Defender Zakar"] = {
+		["Heroic"] = 263,
+	},
+	["Jeweled Azergem Crystalback"] = {
+		["Heroic"] = 563,
+	},
+	["Brimstone Hound"] = {
+		["Normal"] = 6,
+	},
+	["Stonebound Quilen"] = {
+		["Normal"] = 6,
+	},
+	["Crag Rager"] = {
+		["Heroic"] = 18,
+	},
+	["Risen Icetusk Brute"] = {
+		["Heroic"] = 18,
+	},
+	["Slitherblade Gladiator"] = {
+		["Heroic"] = 9,
+	},
+	["Agitated Azershard"] = {
+		["Heroic"] = 15,
+		["Normal"] = 10,
+	},
+	["Icetusk Shadowcaster"] = {
+		["Heroic"] = 9,
+	},
+	["Berserker Gola"] = {
+		["Heroic"] = 488,
+	},
+	["Mo'gosh Mystic"] = {
+		["Normal"] = 12,
+	},
+	["Snowfur Pup"] = {
+		["Heroic"] = 5,
 	},
 	["Unstable Typhoon"] = {
 		["Normal"] = 90,
+		["Heroic"] = 135,
 	},
-	["Azergem Shardback"] = {
-		["Normal"] = 18,
+	["Unbreakable Vortax"] = {
+		["Heroic"] = 263,
 	},
-	["Senior Producer Gixi"] = {
-		["Normal"] = 325,
+	["Guran the Frostblade"] = {
+		["Heroic"] = 263,
 	},
-	["Deepbore"] = {
+	["Brineshell Makrura"] = {
+		["Heroic"] = 9,
+	},
+	["Burning Emberguard"] = {
+		["Heroic"] = 18,
+	},
+	["Slitherblade Prophet"] = {
+		["Heroic"] = 135,
+	},
+	["Deathsting Scorpid"] = {
+		["Heroic"] = 9,
+	},
+	["Cragburster"] = {
+		["Heroic"] = 263,
+	},
+	["Rustpelt Wolf"] = {
+		["Normal"] = 6,
+	},
+	["Slitherblade Saurok"] = {
+		["Heroic"] = 5,
+	},
+	["Ocean Recluse"] = {
+		["Heroic"] = 488,
+	},
+	["Brittle Skeleton"] = {
+		["Heroic"] = 5,
+		["Normal"] = 3,
+	},
+	["Steelshred"] = {
+		["Heroic"] = 263,
+	},
+	["Gargantuan Blighthound"] = {
+		["Heroic"] = 488,
+	},
+	["Fleshrot Vandal"] = {
+		["Heroic"] = 9,
+	},
+	["Snowstalker"] = {
 		["Normal"] = 175,
 	},
-	["Deathwhisperer Kulu"] = {
+	["Knucklebump Silverback"] = {
+		["Normal"] = 90,
+	},
+	["Voru'kar Swarmling"] = {
+		["Heroic"] = 5,
+	},
+	["Vilegaze Petrifier"] = {
+		["Heroic"] = 488,
+	},
+	["Voru'kar Nerubian"] = {
+		["Heroic"] = 18,
+	},
+	["Shadefeather Hatchling"] = {
+		["Normal"] = 3,
+	},
+	["Baron Blazehollow"] = {
+		["Heroic"] = 488,
+	},
+	["Faceted Earthbreaker"] = {
+		["Heroic"] = 488,
+	},
+	["Spineshell Snapper"] = {
+		["Heroic"] = 5,
+	},
+	["Suffused Azerite"] = {
+		["Normal"] = 375,
+	},
+	["Azergem Crystalback"] = {
+		["Heroic"] = 180,
+		["Normal"] = 120,
+	},
+	["Gritplate Crystaleye"] = {
+		["Normal"] = 90,
+		["Heroic"] = 135,
+	},
+	["Snowfur Snarler"] = {
+		["Heroic"] = 18,
+	},
+	["Snowfur Alpha"] = {
+		["Heroic"] = 135,
+	},
+	["Bartok the Burrower"] = {
+		["Heroic"] = 263,
+	},
+	["Tidal Surger"] = {
+		["Normal"] = 12,
+		["Heroic"] = 18,
+	},
+	["Stonebound Warrior"] = {
+		["Heroic"] = 18,
+		["Normal"] = 12,
+	},
+	["Jadescale Gnasher"] = {
+		["Heroic"] = 18,
+	},
+	["Azerite Chunk"] = {
+		["Heroic"] = 143,
+		["Normal"] = 95,
+	},
+	["Fire Elemental"] = {
+		["Heroic"] = 9,
+	},
+	["Quilldozer"] = {
+		["Heroic"] = 488,
+	},
+	["Jared the Jagged"] = {
+		["Normal"] = 175,
+	},
+	["Coralback Surfcrawler"] = {
+		["Heroic"] = 18,
+	},
+	["General Drakkarion"] = {
+		["Heroic"] = 488,
+	},
+	["Fire Lord's Cache"] = {
+		["Heroic"] = 225,
+	},
+	["Voru'kar Infector"] = {
+		["Heroic"] = 9,
+	},
+	["Agitated Azerite"] = {
+		["Normal"] = 10,
+	},
+	["Breakbeak Scavenger"] = {
+		["Heroic"] = 18,
+	},
+	["Wooden Strongbox"] = {
+		["Heroic"] = 68,
+		["Normal"] = 45,
+	},
+	["Mischievous Flood"] = {
+		["Heroic"] = 18,
+	},
+	["Frostscale Hydra"] = {
+		["Heroic"] = 18,
+	},
+	["Fleshrot Marauder"] = {
+		["Heroic"] = 18,
+	},
+	["Shadefeather Raven"] = {
+		["Normal"] = 12,
+	},
+	["Broken Azerite Shard"] = {
+		["Heroic"] = 38,
+	},
+	["Jadescale Worm"] = {
+		["Heroic"] = 9,
+	},
+	["Nightscale Screecher"] = {
+		["Heroic"] = 18,
+		["Normal"] = 12,
+	},
+	["Bramblefur Herdleader"] = {
+		["Heroic"] = 488,
+	},
+	["Stonebound Rockmaw"] = {
+		["Heroic"] = 18,
+	},
+	["Plundered Azerite"] = {
+		["Heroic"] = 225,
+	},
+	["Brineshell Minor Oracle"] = {
+		["Heroic"] = 263,
+	},
+	["Venomlash"] = {
+		["Heroic"] = 263,
+	},
+	["Mo'gosh Magi"] = {
+		["Normal"] = 6,
+	},
+	["Monstrous Shadefeather"] = {
+		["Normal"] = 90,
+	},
+	["Congealed Azerite"] = {
+		["Normal"] = 10,
+	},
+	["Venomscale Hydra"] = {
+		["Heroic"] = 18,
+	},
+	["Luminous Azerite"] = {
+		["Normal"] = 120,
+		["Heroic"] = 180,
+	},
+	["Earth Elemental"] = {
+		["Heroic"] = 9,
+	},
+	["Slitherblade Ironscale"] = {
+		["Heroic"] = 18,
+	},
+	["Sack of Azerite"] = {
+		["Heroic"] = 173,
+		["Normal"] = 115,
+	},
+	["Garnetback Worm"] = {
+		["Heroic"] = 9,
+	},
+	["Spineshell Turtle"] = {
+		["Heroic"] = 9,
+	},
+	["Zu-Xan of Thunder"] = {
+		["Normal"] = 175,
+	},
+	["Spineclaw Rockshell"] = {
+		["Heroic"] = 18,
+	},
+	["Twilight Drake"] = {
+		["Heroic"] = 18,
+	},
+	["Azerite Shard"] = {
+		["Heroic"] = 83,
+		["Normal"] = 55,
+	},
+	["Deepstone Crusher"] = {
+		["Heroic"] = 135,
+	},
+	["Knucklebump Gorilla"] = {
+		["Normal"] = 6,
+	},
+	["Shathhoth the Punisher"] = {
 		["Normal"] = 325,
+	},
+	["Zian-Ti Brutalizer"] = {
+		["Normal"] = 12,
+	},
+	["Runehoof Courser"] = {
+		["Heroic"] = 18,
+	},
+	["Stone Machinist Nu-Xin"] = {
+		["Heroic"] = 488,
+	},
+	["Unleashed Azerite"] = {
+		["Heroic"] = 27,
+		["Normal"] = 18,
+	},
+	["Mo'gosh Arcanist"] = {
+		["Normal"] = 90,
+	},
+	["Ghoul"] = {
+		["Heroic"] = 9,
+	},
+	["Bramblefur Bull"] = {
+		["Heroic"] = 135,
+	},
+	["Crystalline Giant"] = {
+		["Normal"] = 325,
+	},
+	["Screeching Nightwing"] = {
+		["Heroic"] = 135,
 	},
 }
